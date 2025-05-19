23490d542c03705261a4ffd3985e6972c5ef3d0a
sakastudio <katsumi.sato@aktsk.jp>
2025-05-19 18:23:21 +0900

    上にコマンド追加、下にコマンド追加

--- frontend/e2e/basic.spec.ts
+++ frontend/e2e/basic.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Group commands', async ({ page }) => {
   await page.goto('http://localhost:5173/');
   await page.getByTestId('sortable-item-1').click();
   await page.getByTestId('sortable-item-3').click({
@@ -10,6 +10,31 @@ test('test', async ({ page }) => {
     button: 'right'
   });
   await page.getByRole('menuitem', { name: 'グループ化' }).click();
+
+  await expect(page).toHaveScreenshot();
+});
+
+
+test('Add a command above the selected command', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.getByTestId('sortable-item-2').click({
+    button: 'right'
+  });
+  await page.getByRole('menuitem', { name: '上にコマンドを追加' }).click();
+  await page.getByRole('menuitem', { name: '待機' }).click();
+
+  await expect(page).toHaveScreenshot();
+});
+
+
+test('Add a command below the selected command', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.getByTestId('sortable-item-2').click({
+    button: 'right'
+  });
+
+  await page.getByRole('menuitem', { name: '下にコマンドを追加' }).click();
+  await page.getByRole('menuitem', { name: '待機' }).click();
 
   await expect(page).toHaveScreenshot();
 });
