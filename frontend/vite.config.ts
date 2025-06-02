881767ead0722636dd35d9a9d5d52c9ddf84c210
sakastudio / Katsumi Sato <55620461+sakastudio@users.noreply.github.com>
2025-06-02 16:14:18 +0900

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,13 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [{ src: 'src/sample/*', dest: 'sample' }]
+    })
+  ],
+  
   base: process.env.GITHUB_ACTIONS ? '/CommandForgeEditor/' : '/',
   resolve: {
     alias: {
